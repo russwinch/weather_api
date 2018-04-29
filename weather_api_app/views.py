dff92d73811f20d7ecbb79693f2fd183bca584f5
George Kaimakis <geoptus@gmail.com>
2018-04-29 16:50:54 +0100

    Adds module brief docstring as description:

--- weather_api_app/views.py
+++ weather_api_app/views.py
@@ -1,4 +1,3 @@
-'''
-This is where the routes are defined. It may be split into a package of its 
-own (yourapp/views/) with related views grouped together into modules.
-'''
+"""
+A Flask application to provide an  api interphase via a secure login page
+"""
