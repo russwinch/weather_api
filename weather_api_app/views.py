5adcc742c66f1811cc24a2497565bd36f841a925
George Kaimakis <geoptus@gmail.com>
2018-04-29 22:10:15 +0100

    Adds html and css code, update to views.py:
    bootstrap css added to static directory
    index and login html code added to templates directory
    'route' links to both index & login added to views.py
    
    Authentication logic to be added to login function

--- weather_api_app/views.py
+++ weather_api_app/views.py
@@ -1,18 +1,29 @@
 """
-A Flask application to provide an api interface via a secure login page
+A Flask application to provide the weather api interface via a secure
+login page
 """
 
 
-from flask import Flask
+from flask import Flask, render_template
 
 
 # create the application object:
 app = Flask(__name__)
 
 
+# use decorators to link the function to a url:
 @app.route("/")
 def main_page():
-    return "Weather API - Main Page"
+    """calls the web site home page"""
+    return render_template("index.html")
+
+
+#  route for handling the login page logic:
+@app.route("/login", methods=["GET", "POST"])
+def login_page():
+    """calls the web site login page"""
+    # authentication to be added:
+    return render_template("login.html")
 
 
 if __name__ == "__main__":
