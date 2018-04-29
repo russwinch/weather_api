5f2cac3f818ea11ded84067ce1293ea11e831f1f
George Kaimakis <geoptus@gmail.com>
2018-04-29 17:56:57 +0100

    Basic flask app page (no links):
    Renders a simple web page without any html code via the 'return' of the
    main_page function.
    'debug' mode enabled via the app server run-time
    
    To render the page, run the Flask server using 'python views.py' in a
    terminal and open a browser at 'localhost:5000'

--- weather_api_app/views.py
+++ weather_api_app/views.py
@@ -1,3 +1,19 @@
 """
-A Flask application to provide an  api interphase via a secure login page
+A Flask application to provide an api interface via a secure login page
 """
+
+
+from flask import Flask
+
+
+# create the application object:
+app = Flask(__name__)
+
+
+@app.route("/")
+def main_page():
+    return "Weather API - Main Page"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
