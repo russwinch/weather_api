5557573b14e71cbadef7aa03d8464864f924b759
George Kaimakis <geoptus@gmail.com>
2018-05-08 01:21:56 +0100

    amends view.py logic, adds welcome.html:
    basic navigation between template pages,
    index.html
    login.html
    welcome.html
    
    user login without authentication
    no login integrety for secure pages

--- weather_api_app/views.py
+++ weather_api_app/views.py
@@ -4,26 +4,51 @@ login page
 """
 
 
-from flask import Flask, render_template
+from flask import Flask, flash, render_template, redirect, request,\
+                  url_for, session
 
 
 # create the application object:
 app = Flask(__name__)
 
+app.secret_key = "my_precious" # this key must be relocated to instance/config
+
 
 # use decorators to link the function to a url:
-@app.route("/")
-def main_page():
+@app.route('/')
+def home():
     """calls the web site home page"""
     return render_template("index.html")
 
 
 #  route for handling the login page logic:
-@app.route("/login", methods=["GET", "POST"])
-def login_page():
+@app.route('/login', methods=["GET", "POST"])
+def login():
     """calls the web site login page"""
-    # authentication to be added:
-    return render_template("login.html")
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or \
+                request.form['password'] != 'admin':
+            error = 'Invalid credentials. Please try again.'
+        else:
+            session['logged_in'] = True
+            flash('You are logged in!')
+            return redirect(url_for('welcome'))
+    return render_template('login.html', error=error)
+
+
+@app.route('/welcome')
+def welcome():
+    """calls the web site welcome page"""
+    return render_template('welcome.html')
+
+
+@app.route('/logout')
+def logout():
+    """calls  the logout logic"""
+    session.pop('logged_in', None)
+    flash('You were logged out!')
+    return redirect(url_for('login'))
 
 
 if __name__ == "__main__":
