818e655de0b9e0fa13401891392bba49c231111b
russ <russell.winch@gmail.com>
2018-06-01 08:29:11 +0100

    Moved the printing of latest weather.

--- run_darksky_get.py
+++ run_darksky_get.py
@@ -10,36 +10,33 @@ import json
 from darksky import DarkSky
 
 
-def read_local_darksky(file_in, print_summary=True):
+def read_local_darksky(file_in):
     """
     Returns a parsed dict from the local text file.
 
     :file_in: the location of the file with the forecast json
-    :print_summary: a boolean which controls if the summary forecast will be
-                    printed after retrieval
     """
-    with open(file_in) as f:
-        weather_json = f.read()
-    weather_dict = json.loads(weather_json)
-    if print_summary:
-        print("Weather summary:\n{}".format(weather_dict['daily']['summary']))
+    with open(file_in) as f_in:
+        weather_json = f_in.read()
 
+    weather_dict = json.loads(weather_json)
     return weather_dict
 
 
 def update_darksky(file_out):
     """
-    Requests and returns a response from DarkSky using instance config.
+    Requests and returns a response from DarkSky using instance config,
+    storing it locally in a file as text.
 
     :file_out: the location of the file where the forecast json will be stored
     """
     dark_sky = DarkSky(key=instance.config.DARK_SKY_API_KEY)
     r = dark_sky.request(latitude=instance.config.LATITUDE,
                          longitude=instance.config.LONGITUDE)
-    with open(file_out, mode='w') as out:
-        out.write(r.text)
-    print("Updated and written to local file.")
+    with open(file_out, mode='w') as f_out:
+        f_out.write(r.text)
 
+    print("Updated and written to local file.")
     return r
 
 
@@ -48,3 +45,4 @@ if __name__ == '__main__':
 
     weather_response = update_darksky(weather_file)
     weather_dict = read_local_darksky(weather_file)
+    print("Weather summary:\n{}".format(weather_dict['daily']['summary']))
