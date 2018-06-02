2c49ed01cb1aea394b5228d09ed309fe6df8e3d4
russ <russell.winch@gmail.com>
2018-06-02 11:12:48 +0100

    Minor change to print the minutely forecast. Daily was still returning yesterday as the first item. This will need to be taken into consideration when trying to determine todays forecast.

--- run_darksky_get.py
+++ run_darksky_get.py
@@ -41,8 +41,8 @@ def update_darksky(file_out):
 
 
 if __name__ == '__main__':
-    weather_file = 'darksky.json'
+    weather_file = 'darksky.json'  # this should move to the config
 
     weather_response = update_darksky(weather_file)
     weather_dict = read_local_darksky(weather_file)
-    print("Weather summary:\n{}".format(weather_dict['daily']['summary']))
+    print("Weather summary:\n{}".format(weather_dict['minutely']['summary']))
